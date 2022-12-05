4d8bde41c6a5d4426b1c21e5e1e3e8330373210f
Chris Loveless <chriskloveless@gmail.com>
2022-12-05 11:48:17 -0800

    add GetAllWheel method and WIP: SalePrice method  using percentage

--- Models/Car.cs
+++ Models/Car.cs
@@ -8,13 +8,22 @@ namespace Dealership.Models
     public string MakeModel { get; set; }
     public int Price { get; set; }
     public int Miles { get; set; }
+    public bool AllWheel { get; set; }
 
     // Constructor
-    public Car(string makeModel, int price, int miles)
+    public Car(string makeModel, int price, int miles, bool allWheel)
     {
         MakeModel = makeModel;
         Price = price;
         Miles = miles;
+        AllWheel = allWheel;
+    }
+
+    public void SalePrice(int discount)
+    {
+      int priceSub = Price * (discount/100); 
+      Price = Price - priceSub;
+      // Price = Price - discount; ---> code for discount without percentage <---
     }
 
     public void SetPrice(int newPrice)
@@ -23,11 +32,10 @@ namespace Dealership.Models
     }
 
     public static string MakeSound(string sound)
-        {
-            return "Our cars sound like " + sound;
-        }
+    {
+      return "Our cars sound like " + sound;
+    }
     
-
     public string GetMakeModel()
     {
       return MakeModel;
@@ -41,6 +49,11 @@ namespace Dealership.Models
     public int GetMiles()
     {
       return Miles;
+    }
+
+    public bool GetAllWheel()
+    {
+      return AllWheel;
     }
 
     public bool WorthBuying(int maxPrice)
--- Program.cs
+++ Program.cs
@@ -8,26 +8,34 @@ namespace Dealership
   {
     public static void Main()
     {
-      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-      Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-      Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-      Car amc = new Car("1976 AMC Pacer", 400, 198000);
+      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, false);
+      Car yugo = new Car("1980 Yugo Koral", 700, 56000, false);
+      Car ford = new Car("1988 Ford Country Squire", 1400, 239001, false);
+      Car amc = new Car("1976 AMC Pacer", 400, 198000, false);
+      Car subi = new Car("1996 Subaru Legacy", 800, 215000, true);
 
-      List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+      List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc, subi };
 
       Console.WriteLine("Enter maximum price: ");
       string stringMaxPrice = Console.ReadLine();
       int maxPrice = int.Parse(stringMaxPrice);
+      Console.WriteLine("All Wheel Drive? ['Y' for yes, 'Enter' for no]");
+      string awdString = Console.ReadLine();
 
       List<Car> CarsMatchingSearch = new List<Car>(0);
 
       yugo.SetPrice(300);
+      amc.SalePrice(25);
 
       foreach (Car automobile in Cars)
       {
         if (automobile.WorthBuying(maxPrice))
         {
           CarsMatchingSearch.Add(automobile);
+        }
+         if (automobile.GetAllWheel() == false && awdString == "y" || awdString == "Y")
+        {
+          CarsMatchingSearch.Remove(automobile);
         }
       }
 
@@ -36,6 +44,7 @@ namespace Dealership
         Console.WriteLine("----------------------");
         Console.WriteLine(automobile.GetMakeModel());
         Console.WriteLine(automobile.GetMiles() + " miles");
+        Console.WriteLine("All Wheel Drive?:" + " " + automobile.GetAllWheel());
         Console.WriteLine("$" + automobile.GetPrice());
       }
     }
